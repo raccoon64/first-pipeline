b3de994bf4f357b652d0e71576c7b7962ad6e7ce
raccoon64 <wager.raccon@gmail.com>
2019-09-08 09:55:10 +0630

    Hay

---
+++ src/main/java/com/example/firstpipeline/FirstPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstPipelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstPipelineApplication.class, args);
+    }
+
+}
